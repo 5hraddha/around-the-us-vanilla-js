0b0af40cd28db01a554082c3980b4a4403656a9a
Shraddha <mailmeatshraddha@gmail.com>
2021-09-28 10:59:13 -0400

    feat: create Section class that takes ready-made markup and adds it to the DOM

---
+++ scripts/Section.js
@@ -0,0 +1,43 @@
+/**
+ * This module takes care of rendering a list of elements on a page
+ * @module Section
+ */
+
+/** Class representing a Layer that takes ready-made markup and adds it to the DOM. */
+class Section {
+  /**
+   * Creates a new layer.
+   * @param {Object} obj - The object has two properties - `items` and `renderer`. The `items` property serve as an array of data and the `renderer` property is a function responsible for creating and rendering data on a page.
+   * @param {string} containerSelector -  The CSS class selector to select the container for adding the card elements.
+   */
+  constructor({ items, renderer}, containerSelector) {
+    this._renderedItems = items;
+    this._renderer      = renderer;
+    this._container     = document.querySelector(containerSelector);
+  }
+
+  /**
+   * Takes a DOM element and adds it to the container.
+   * @param {HTMLElement} element The HTML Element to add to the container.
+   */
+  addItem(element) {
+    this._container.append(element);
+  }
+
+  /**
+   * Clears the container of all the child elements.
+   */
+  clear() {
+    this._container.innerHTML = "";
+  }
+
+  /**
+   * Iterates through the array of data elements and renders each element on a page.
+   */
+  renderItems() {
+    this.clear();
+    this._renderedItems.forEach(item => this._renderer(item));
+  }
+}
+
+export default Section;
